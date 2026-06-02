8dd8815b2d0e985de8996c76ba7ee0e2ab41cc6a
Jeff Sokol <JeffSokolDev@gmail.com>
2026-06-02 19:11:02 -0400

    start of leage game pages + drilling information

--- app/page.js
+++ app/page.js
@@ -1,19 +1,32 @@
-import { getAllData, getWeekThreeData } from "@/lib/nfl";
+import { getAllNFLData, getNFLTeams, getTeamSchedule } from "@/lib/nfl";
+import { getAllMLBData } from "@/lib/mlb";
+import { getAllMLSData } from "@/lib/mls";
+import { getAllNBAData } from "@/lib/nba";
+import { getAllNCAAFData } from "@/lib/ncaaf";
+import { getAllNCAAMData } from "@/lib/ncaam";
+import { getAllNHLData } from "@/lib/nhl";
+import { getAllUSWNTData } from "@/lib/uswnt";
+import { getAllWBNAData } from "@/lib/wbna";
 
-const [nflData, week3Data] = await Promise.all([getAllData(), getWeekThreeData()])
-// console.log('allData', nflData)
-// console.log('drilled', nflData.leagues[0]) JUST LEAGUE INFORMATION
-// console.log('entries', nflData.leagues[0].calendar[0].entries)
 
-console.log('week3', week3Data)
+
+
+const [nflData, nflTeams, mlbData, mlsData, nbaData, ncaafData, ncaamData, nhlData, uswntData, wnbaData] = await Promise.all([getAllNFLData(), getNFLTeams(),
+      getAllMLBData(),getAllMLSData(), getAllNBAData(), getAllNCAAFData(), getAllNCAAMData(), getAllNHLData(), getAllUSWNTData(), getAllWBNAData() ])
+      // console.log('nflTeams', nflTeams)
+
+const scheduleData = await getTeamSchedule(30)
+// console.log('niners Sched', scheduleData)
+console.log( 'Sched drill', scheduleData.events[2].competitions[0].venue)
+
 
 export default function Home() {
 
   return (
     <div>
       <h1>FIELDTRIP</h1>
-      <h2>DATA FOR {nflData.leagues[0].name}</h2>
-      {nflData.leagues[0].calendar.map((single) =>(
+      {/* <h2>DATA FOR {nflData.leagues[0].name}</h2> */}
+      {/* {nflData.leagues[0].calendar.map((single) =>(
         <div key={single.value}>
           <p>{single.label}</p>
           <p>{new Date(single.startDate).toLocaleDateString()} - {new Date(single.endDate).toLocaleDateString()}</p>
@@ -28,7 +41,22 @@ export default function Home() {
           <p>{singleEvent.competitions[0].venue.fullName}</p>
           <p>{singleEvent.competitions[0].venue.address.city},{singleEvent.competitions[0].venue.address.state} {singleEvent.competitions[0].venue.address.country}</p>
         </div>
-      ))}
+      ))} */}
+    {/* NFL TEAMS */}
+    {/* {nflTeams.items.map((team) => (
+      <div key = {team.id}>
+        <h2>{team.displayName}</h2>
+        </div>
+    ))} */}
+    {/* SAMPLE SCHEDULE */}
+    <h1>{scheduleData.displayName} Schedule Data</h1>
+    {scheduleData.events.map((event) => (
+      <div key={event.id}>
+        <p>{event.shortName}</p>
+        <p>Location:{event.competitions[0].venue.fullName}</p>
+        <p>{event.competitions[0].venue.address.city} - {event.competitions[0].venue.address.country}</p>
+      </div>
+    ))}
     </div>
   );
 }
